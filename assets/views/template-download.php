13b48ae149e9698b67c3b06557869530c8b6a072
Trevor Wagner <trevorwagner@Trevors-MBP.attlocal.net>
2022-04-22 19:31:56 -0400

    creatign download overlay

--- assets/views/template-download.php
+++ assets/views/template-download.php
@@ -3,8 +3,10 @@
 if( is_array($download) ){
 
     $html .= '<div>
-                <a href="' . $download['filename'] . '" download="' . $download['nicename'] . '">
+                <a href="' . $download['filename'] . '" download="' . $download['nicename'] . '" title="Download ' . $download['nicename'] . '">
                     <img src="' . $download['imageUrl'] . '" alt="' . $download['imageAlt'] . '" title="' . $download['imageTitle'] . '" />
+                    <div class="title">' . $download['title'] . '</div>
+                    <span class="overlay"><i class="fa fa-fw fa-download"></i></span>
                 </a>
               </div>';
 }
