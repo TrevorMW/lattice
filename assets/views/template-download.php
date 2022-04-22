7640ddbad8333d03835a0eeb5e0072b635a8667a
Trevor Wagner <trevorwagner@Trevors-MacBook-Pro.local>
2022-04-22 09:14:25 -0400

    finishing deep dive tabs

--- assets/views/template-download.php
+++ assets/views/template-download.php
@@ -2,13 +2,11 @@
 
 if( is_array($download) ){
 
-    if( $download['type'] === 'link' ){
-        $html .= '<a href="' . $download['filename'] . '" download="' . $download['nicename'] . '"><i class="fa fa-fw fa-download"></i>&nbsp;' . $download['title'] . '</a>';
-    }
-
-    if( $download['type'] === 'image' ){
-        $html .= '<div><a href="' . $download['filename'] . '" download="' . $download['nicename'] . '"><img src="' . $download['image']['url'] . '" alt="' . $download['image']['alt'] . '" title="' . $download['image']['title'] . '" /></a></div>';
-    }
+    $html .= '<div>
+                <a href="' . $download['filename'] . '" download="' . $download['nicename'] . '">
+                    <img src="' . $download['imageUrl'] . '" alt="' . $download['imageAlt'] . '" title="' . $download['imageTitle'] . '" />
+                </a>
+              </div>';
 }
 
 return $html;
