04435ef3229c8f1470dfadf240e86e55c63675f8
Trevor Wagner <trevorwagner@Trevors-MacBook-Pro.local>
2022-06-01 11:00:26 -0400

    adding quotes

--- footer.php
+++ footer.php
@@ -93,7 +93,7 @@ if($gtmCode){ ?>
 	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-	})(window,document,'script','dataLayer', <?php echo $gtmCode?> );</script>
+	})(window,document,'script','dataLayer', '<?php echo $gtmCode?>' );</script>
 	<!-- End Google Tag Manager -->
 <?php } ?>
 
